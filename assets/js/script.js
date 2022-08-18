83740c98827ff0fac9839f2f77a20a07a734f8e5
Mari-marina <83422160+Mari-marina@users.noreply.github.com>
2022-08-18 14:24:50 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+
+
+let anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    let blockID = anchor.getAttribute("href");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+if (document.querySelector(".icon-menu")) {
+  document.querySelector(".icon-menu").addEventListener("click", function (e) {
+    document.querySelector("body").classList.toggle("lock");
+    document.documentElement.classList.toggle("menu-open");
+  });
+}
+
+// if (document.querySelector(".main__wrapper")) {
+//   document.querySelector(".main__wrapper").addEventListener("click", function (e) {
+//     document.querySelector("body").classList.toggle("lock");
+//     document.documentElement.classList.toggle("menu-open");
+//   });
+// }
+
+const mm = document.querySelector(".mm");
+const form1 = document.querySelector(".request__form-mobile");
+const form2 = document.querySelector(".request__form-mobile.two");
+const formBtn = document.querySelector(".mobile__BTNtext");
+const formBtn2 = document.querySelector(".mobile__BTNtext.two");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navLinks.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    document.querySelector("body").classList.remove("lock");
+    document.documentElement.classList.remove("menu-open");
+  });
+});
+
+mm.addEventListener("click", function (e) {
+  document.querySelector("body").classList.remove("lock");
+  document.documentElement.classList.remove("menu-open");
+});
+
+// navigation.addEventListener("click", function (e) {
+//   document.querySelector("body").classList.remove("lock");
+//   document.documentElement.classList.remove("menu-open");
+// });
+
+formBtn.addEventListener("click", function (e) {
+  formBtn.classList.add("clicked");
+  formBtn2.classList.remove("clicked");
+  form2.classList.remove("active");
+  form1.classList.add("active");
+});
+formBtn2.addEventListener("click", function (e) {
+  formBtn2.classList.add("clicked");
+  formBtn.classList.remove("clicked");
+  form2.classList.add("active");
+  form1.classList.remove("active");
+});
+
+//# sourceMappingURL=script.js.map
